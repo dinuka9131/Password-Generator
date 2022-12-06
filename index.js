878cc6d6b9b6ee5f236433cf9b39be7150e1d451
dinuka9131 <119585225+dinuka9131@users.noreply.github.com>
2022-12-06 09:46:43 +0530

    update js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M",
 "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
 "@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
 ",","|",":",";","<",">",".","?",
-"/"];
+"/"]
 
 let passwordOne = document.getElementById("password-1")
 let passwordTwo = document.getElementById("password-2")
