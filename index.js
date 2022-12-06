13cf3f64d2138ce8ca708e888c0c24b53fe0d1a4
dinuka9131 <119585225+dinuka9131@users.noreply.github.com>
2022-12-06 09:36:47 +0530

    password-generator

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M",
+"N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
+"f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+"x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
+"@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
+",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordOne = document.getElementById("password-1")
+let passwordTwo = document.getElementById("password-2")
+
+
+
+
+function generate(){
+    passwordOne.textContent = ""
+    for(let i = 0; i < 15; i++){
+        let i = Math.floor(Math.random()*characters.length)
+        passwordOne.textContent += characters[i]   
+    }
+    passwordTwo.textContent = ""
+    for(let j = 0; j < 15; j++){
+        let j = Math.floor(Math.random()*characters.length)
+        passwordTwo.textContent += characters[j]   
+    }
+}
+
+
+
